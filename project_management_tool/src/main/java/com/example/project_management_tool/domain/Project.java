69c73518f1c55ce323fce40008372fdcc3f2449d
Yatin Relhan <yrelhan4@gmail.com>
2019-12-19 02:51:50 -0600

    project controller logic

--- project_management_tool/src/main/java/com/example/project_management_tool/domain/Project.java
+++ project_management_tool/src/main/java/com/example/project_management_tool/domain/Project.java
@@ -1,7 +1,11 @@
 package com.example.project_management_tool.domain;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -10,8 +14,13 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "Project name is required")
     private String projectName;
+    @NotBlank(message = "Project identifier is required")
+    @Size(min=4, max = 5, message = "Please use 4 to 5 characters")
+    @Column(updatable = false,unique = true)
     private String projectIdentifier;
+    @NotBlank(message = "Project description is required")
     private String description;
 
     public String getDescription() {
@@ -22,10 +31,13 @@ public class Project {
         this.description = description;
     }
 
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date start_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_date;
-
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date created_At;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updated_At;
 
     public Project(){
--- project_management_tool/src/main/java/com/example/project_management_tool/web/ProjectController.java
+++ project_management_tool/src/main/java/com/example/project_management_tool/web/ProjectController.java
@@ -6,10 +6,17 @@ import com.example.project_management_tool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -19,9 +26,18 @@ public class ProjectController {
     private ProjectService projectService;
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+
+        if(result.hasErrors()){
+
+            Map<String,String> errorMap = new HashMap<>();
+            for(FieldError error: result.getFieldErrors()){
+                errorMap.put(error.getField(),error.getDefaultMessage());
+            }
+
+            return new ResponseEntity<Map<String,String>>(errorMap,HttpStatus.BAD_REQUEST);
+        }
         Project project1 = projectService.saveOrUpdateProject(project);
         return  new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
-
 }
