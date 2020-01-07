67a149a4e7f939d7cc66bf3a0bd2d49569dc4c6e
Yatin Relhan <yrelhan4@gmail.com>
2020-01-07 00:59:41 -0600

    Project Management Tool Web Application

---
+++ ppmtool-react-client/build/precache-manifest.c93651b801324ea3c629dcc87634e1a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14c4db78ce55616e3981830666854552",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6eee0db3042bb5e98fcd",
+    "url": "/static/css/2.d9253284.chunk.css"
+  },
+  {
+    "revision": "28f0ddcbccbe0ca9b083",
+    "url": "/static/css/main.7be3ab8b.chunk.css"
+  },
+  {
+    "revision": "6eee0db3042bb5e98fcd",
+    "url": "/static/js/2.5e64be51.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.5e64be51.chunk.js.LICENSE"
+  },
+  {
+    "revision": "28f0ddcbccbe0ca9b083",
+    "url": "/static/js/main.a1c6cfc4.chunk.js"
+  },
+  {
+    "revision": "0e0de284a6b544b8ab0b",
+    "url": "/static/js/runtime-main.df9c3e05.js"
+  }
+]);
